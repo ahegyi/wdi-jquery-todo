7315dffedae94a45f5f4841ecdea68bf5122bc49
Aron Hegyi <ahegyi@gmail.com>
2013-04-30 08:24:43 -0700

    Add delete functionality

--- todo.js
+++ todo.js
@@ -38,7 +38,9 @@ $(document).ready(function() {
     if (type === "completed") {
       checked = ' checked="checked"';
     }
-    return $('<li><input type="checkbox" id="' + type + 'Checkbox' + index + '"' + checked + '><span>' + text + '</span></li>');
+    return $('<li>' + 
+      '<input type="checkbox" id="' + type + 'Checkbox' + index + '"' + checked + '><span>' +
+      text + '</span>' + '<button id="'+ type + 'Delete' + index + '">Delete</button>' + '</li>');
   }
 
   function updateLists() {
@@ -55,6 +57,10 @@ $(document).ready(function() {
         newTodo.children('input[type=checkbox]').on('click', function(e) {
           unCompleteItem(e.target);
         });
+        newTodo.children('button').on('click', function (e) {
+          //($(e.target).parent('li')).hide( "drop", { direction: "up" }, "slow", function () { deleteItem(e.target, "completed"); } );
+          deleteItem(e.target, "completed");
+        });
         completedCount += 1;
       }
       else {
@@ -62,6 +68,10 @@ $(document).ready(function() {
         newTodo.appendTo(todoEl);
         newTodo.children('input[type=checkbox]').on('click', function(e) {
           completeItem(e.target);
+        });
+        newTodo.children('button').on('click', function (e) {
+          //($(e.target).parent('li')).hide( "drop", { direction: "up" }, "slow", function () { deleteItem(e.target, "todo"); } );
+          deleteItem(e.target, "todo");
         });
         todoCount += 1;
       }
@@ -90,6 +100,23 @@ $(document).ready(function() {
     var index = parseInt(el.id.replace("completedCheckbox", ""), 10);
     todoArr[index].completed = false;
     updateLists();
+  }
+
+  function deleteItem(el, type) {
+    var indexToDelete = parseInt(el.id.replace(type + "Delete", ""), 10);
+    var newArr = [];
+    var deletedEl;
+    for (var i = 0; i < todoArr.length; i += 1) {
+      if (i === indexToDelete) {
+        deletedEl = todoArr[i];
+      }
+      else {
+        newArr.push(todoArr[i]);
+      }
+    }
+    todoArr = newArr;
+    updateLists();
+    return deletedEl;
   }
 
   completedContainer.hide();
